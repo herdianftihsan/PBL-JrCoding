a913d8445e5a97c865c1b92c97bd038b6a854772
Herdian Immanuel <167686171+herdianftihsan@users.noreply.github.com>
2025-11-21 18:51:53 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,9 @@ function login(event){
    
 
   if (user.role === "admin") {
-    window.location.href = "/PBL-JrCoding/admin/index.html";
+    window.location.href = "/admin/index.html";
 } else if (user.role === "user") {
-    window.location.href = "/PBL-JrCoding/user/index.html";
+    window.location.href = "/user/index.html";
 }
 }
 
